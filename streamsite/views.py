d2a08a131f614063a3ae7e73ed8b98f671cd3ae1
Emman <emman.bg6@gmail.com>
2024-11-01 09:37:55 +0800

    Create views for the remaining models

--- streamsite/views.py
+++ streamsite/views.py
@@ -12,3 +12,43 @@ class UserProfileListView(generics.ListCreateAPIView):
 class UserProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+
+class ChannelListView(generics.ListCreateAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+
+class ChannelDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+
+class SubscriptionListView(generics.ListCreateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+
+class SubscriptionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+
+class VideoListView(generics.ListCreateAPIView):
+    queryset = Video.objects.filter(removed=False)
+    serializer_class = VideoSerializer
+
+
+class VideoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+
+class ContentListView(generics.ListCreateAPIView):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+
+
+class ContentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
